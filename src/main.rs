58a7b0df6ecd8db7100e2d97e39003c8ca7eb08e
SapiensAnatis <jay0@me.com>
2017-04-23 00:47:50 +0100

    Going to use a real algorithm (shunting yard)
    Instead of my own hacked together series of regular expressions, that is

--- src/main.rs
+++ src/main.rs
@@ -4,66 +4,63 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn Substitute(exp: &str, x: &f64) -> f64 {
-    let mut accumulator: f64 = 0f64;
-    let mut currentOp: &str = "";
-    //let mut lastNumber: f64 = 0;
-    // logic
-    let comps = exp.split(" "); // split by whitespace
-    for component in comps {
-        // Handle each component and change the accumulator accordingly. We'll sort of do BIDMAS:
-        // First and foremost, if it's a bracketed expression, we'll need to evaluate that (B):
-        let bracketRegex = Regex::new("(?<=\()\S+(?=\))|(?<=\)\^)\d+"); 
-        /* Let me (attempt) to explain:
+fn EvalulateExp(exp: &str, x: &f64) -> f64 {
+    // Validate expression:
+    expIsValid, reason = IsValidExp(exp);
+    if expIsValid {
+        // Clean up expression to by-the-book infix:
+        exp = InfixAndSubstitute(exp).as_str(); // EnforceInfix returns a String which we'll convert back into a &str
 
-           You have two capturing groups separated by an OR operator as the second one checks for indices,
-           and so is optional. 
-
-           The first one is ((?<=\()\S+(?=\))), which is a lookbehind+ahead for bracket characters,
-           greedily matching any non-whitespace characters (our expression inside the brackets) within.
-           The second one is ((?<=\)\^)\d+), which is again a lookbehind, but this time for a closing bracket character
-           and a hat, then matches the number (power) that the bracket is being raised to. */
-        // So, yeah, bracket logic:
-        if bracketRegex.is_match(component) {
-            
-            // Before adding to the accumulator we need to decide if we're going to raise what we add to any powers:
-            let mut power: u32 = 1;
-
-            let matches = bracketRegex.captures_iter(component);
-            let internalExpression = matches.nth(0).as_str(); // &str
-            if matches.nth(1) != None { // if there was a power
-                // Wait until here to get our string; I believe calling as_str() on None will cause an error
-                power = matches.nth(1).trim().parse()
-                    .expect("Power was not a positive integer number!");
-            } else {
-                power = 1;
-            }
-            
-            powerAccumulator: f64 = 1f64; // Initialize our acc to 1
-            for i in 0..power+1 { // Upper bound is exclusive, so to perform an operation *n* times, we go to n+1
-                powerAccumulator *= Substitute(internalExpression, x); // Multiply it by the value of our bracket
-            }
-
-            accumulator += powerAccumulator; // evaluate the regex match with our existing value of x
-        }
-        // Indices (non-bracket wise) logic (I):
+        // Use a shunting yard algorithm to convert our presumably infix expression into postfix:
         unimplemented!();
     }
-    // return value
-    return accumulator
-    
+    else {
+        print!("Expression is invalid: {}", reason);
+    } 
 }
 
 fn IsValidExp(exp: &str) -> (bool, &str) {
-    otherVariablesRegex = Regex::new("[a-w]|[y-z]");
-    if otherVariablesRegex.is_match(exp) {
-        return (false, "Variables other than x are currently unsupported!");
+    if Regex::new(r"[a-w]|[y-z]").unwrap().is_match(exp) {
+        return (false, "Variables other than x are currently unsupported.");
     }
-    else if (4 == 2) {
+    else if exp.matches("(") != exp.matches(")") {
         // other cases here. example included:
-        return (false, "Maths is currently broken. Please try again later.")
+        return (false, "Unclosed brackets detected.")
     }
     else {
         return (true, "Valid expression!");
     }
+}
+
+fn ShuntingYard(exp: &str) -> Vec<&str> {
+    // Shunting yard
+    unimplemented!();
+}
+
+fn CalculateFromOutput( /* something */ ) -> f64 {
+    // Take our output from Shunting Yard and give a result
+}
+
+fn InfixAndSubstitute(exp: &str, x: &str) -> String {
+    /* Make sure everything is formatted well enough for shunting yard
+       e.g. sin(max(2, 3) / 3 * 3.1415)(2*x) to
+       sin ( max ( 2, 3 ) / 3 * 3.1415 ) * ( 2 * 19 ) 
+    */
+    
+    let mut modifiedExp = String::from(exp.replace(" ", "")); // Create an actual string from our ref string
+    // Remove all whitespace so we can be sure of what we're dealing with
+
+    // We'll first parse x so we have a fully non-algebraic expression:
+    modifiedExp = modifiedExp.replace("x", x.to_string());
+
+    // Turn bracket multiplication into a format understood by the SY algorithm:
+    modifiedExp = modifiedExp.replace(")(", ")*(");
+    // Space out ops/commas/brackets:
+    for c in "+-*/,|^()".chars() {
+        modifiedExp = modifiedExp.replace(c, format!(" {} ", c));
+    }
+    // There will be _some_ double whitespace (bracket multiplication comes to mind), which we'll remove:
+    modifiedExp = Regex::new(r"\s{2,}";).unwrap().replace_all(modifiedExp, " ")
+
+    return modifiedExp;
 }

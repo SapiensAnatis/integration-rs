869479cc01ad97c68c249421b178713f3487b349
SapiensAnatis <jay0@me.com>
2017-04-25 23:12:21 +0100

    Fixed everything-goes-wrong-bug in RPN eval
    Turns out my operands were being used the wrong way around

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ use regex::Regex;
 
 fn main() {
     //let mut input_exp = String::new();
-    let preset_exp = "sin ( max ( 2, 3 ) / 3 * 3.1415 )";
+    let preset_exp = "3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3";
     println!("Please enter an equation of the form y = f(x):\n");
     print!("y = ");
     /*io::stdout().flush();
     io::stdin().read_line(&mut input_exp);*/
-    print!("{}", preset_exp);
+    println!("{}", preset_exp);
     let (is_valid, reason) = expression_is_valid(&preset_exp);
     if is_valid {
         let exp = clean_expression(&preset_exp);
@@ -63,28 +63,36 @@ fn clean_expression(exp: &str) -> String {
     // We'll first parse x so we have a fully non-algebraic expression:
     modified_exp = modified_exp.replace(" ", "");
     // if there is no number in front of x, make it 1*x for simplicity
-    for lone_x in Regex::new(r"[^a\d]x").unwrap().find_iter(exp) {
+    for lone_x in Regex::new(r"[^a\d\)]x").unwrap().find_iter(exp) { // do on exp to stop borrow checker complaining about mutably borrowing below
         // Match x characters directly after operators/brackets/spaces/etc
         // change just 'x' to '1x' so the next bit of code is universal
         // Also only match spaces and newlines in front so as not to get confused with max()
 
         // True start is start+1 since I can't use lookbehinds in Rust
         modified_exp.insert_str(lone_x.start()+1, "1"); 
-        println!("{}", modified_exp);
+    
     }
     modified_exp = modified_exp.replace("x1", "1x"); // for some reason this can happen with powers. temporary hack
-   
+    
     // Now we replace 4x with 4*x so it makes sense. This is why we changed x to 1x, so it's now 1*x.
-    modified_exp = Regex::new(r"[^a]x").unwrap()
-                    .replace_all(&modified_exp, "*x")
-                    .into_owned();
+    for not_a_function_x in Regex::new(r"[^a]x").unwrap().find_iter(exp) {
+        modified_exp.insert_str(not_a_function_x.start()+2, "*");
+    }
+                    
 
     // Have bracket multiplication make sense to SY algorithm:
     modified_exp = modified_exp.replace(")(", ")*(");
-    modified_exp = modified_exp.replace(")x", ")*x");
-    modified_exp = Regex::new(r"[^a]x\(").unwrap() // match x( again without picking on max()
+
+    // modified_exp = modified_exp.replace(")x", ")*x"); Already handled by the most recent regex
+    modified_exp = Regex::new(r"[^a]x\(").unwrap() // match x(, again without picking on max()
                     .replace_all(&modified_exp, "x*(")
                     .into_owned();
+
+    for bracket_coefficient in Regex::new(r"\d\(").unwrap().find_iter(exp) {
+        modified_exp.insert_str(bracket_coefficient.start()+1, "*"); // Change a(x) to a*(x) for comprehension  
+    }
+
+    modified_exp = modified_exp.replace("−", "-"); // Some people like to be special and use a weird minus sign.
 
     for c in r"+-*/,|^()".chars() {
         modified_exp = modified_exp.replace(c, &format!(" {} ", c));
@@ -97,8 +105,18 @@ fn clean_expression(exp: &str) -> String {
     modified_exp = Regex::new(r"\s{2,}").unwrap()
                     .replace_all(&modified_exp, " ")
                     .into_owned(); // Convert COW object into an actual usable String
+        
+    // Subsititute in constants:
+
+    modified_exp = modified_exp.replace(" e ", "2.71828182845");
+    modified_exp = modified_exp.replace(" pi ", "3.14159265359");
+    modified_exp = modified_exp.replace(" π ", "3.14159265359");
 
     return modified_exp;
+}
+
+fn reformat_trace(partial_exp: &String) {
+    println!("Partial reformat: {}", partial_exp);
 }
 
 fn substitute(postfix_exp: Vec<String>, x: &f64) -> Vec<String> {
@@ -106,16 +124,11 @@ fn substitute(postfix_exp: Vec<String>, x: &f64) -> Vec<String> {
     
     for token in postfix_exp {
         if token == "x" {
-            output.push(x.to_string());
-        } else if token == "pi" || token == "π" {
-            output.push("3.14159265359".to_string()); // I mean, I already need it as a string, so why not?
-        } else if token == "e" {
-            output.push("2.71828182845".to_string());
-        }
-        else { output.push(token); }
+            output.push(x.to_string()); 
+        } else { output.push(token); }
     }
     
-    return output
+    return output;
 }
 
 // Shunting yard & component funcs. Some are also used in RPN.
@@ -185,8 +198,7 @@ fn shunting_yard(exp: &str) -> Vec<String> {
             }
              
         } else { println!("Syntax error! Token {} was not identified as any type of token. Therefore, the resulting RPN expression is probably incorrect", token); }
-        //println!("\nFinished parsing token {}", token);
-        //stack_trace(&stack, &output);
+
     } 
     
     for _ in stack.clone() {
@@ -238,85 +250,88 @@ fn convert_str_vec(vec: &Vec<&str>) -> Vec<String> {
 fn evaluate_postfix(stack: &mut Vec<String>) -> f64 {
     let mut output: Vec<f64> = Vec::new();
 
-    for token in stack.clone() { // Process
-        if token_is_number(token.as_str()) {
-            output.push(token.parse::<f64>().expect("Failed to convert numerical token to f64"));
-        } 
-        
-        else { // The token is an op/func
-            let num_args = match token.as_str() { // How many args does our function/op need?
-                // Because we're including operators, having one argument is actually outside of the norm:
-                "sin" | "cos" | "tan" | "ln" | "sqrt" | "cbrt" | "floor" | "ceil" | "round" | "trunc" | "frac" => 1,
-                // (consdier +, -, *, etc which all need two)
-                _ => 2, // none of those supported will need 3 as far as I know
-            };
-
-            if output.len() < num_args {
-                // There was a syntax error; the values needed could not be found
-                println!("Syntax error encountered: (not enough args for token {}: needed {} but only found {} remaining values in stack).\nThe result will most likely be incorrect.", token, num_args, stack.len());
-                // TODO: handle these errors properly
-            }
-            else {
-                let mut operands: Vec<f64> = Vec::new();
-                for v in 0..num_args {
-                    // Pop n values off of the stack (the range upper limit is exclusive, so we start at 0)               
-                    let operand = output.pop().expect("Failed to pop stack into operands.");
-                    operands.push(operand);
-                }
-                // Evalulate operator:
-                let result: f64 = match token.as_str() {
-                    // This is the equivalent of what an eval() call would do normally.
-                    // Except, we didn't include the "exploit" case, so they can't. Much safer.
-
-                    // At the very least, I've implemented most of the functions provided by f64.
-                    // Most meaning I've excluded those easily done by operators like exp2(self) which returns 2^self. 
-
-                    // That doesn't include ln, sqrt, and cbrt which can easily be done through powers, but are
-                    // still commonplace in most expressions. Those are widely used, so we'll allow them to ensure
-                    // compatibility and ease of use.
-
-                    // Basic ops:
-                    "*" => operands[0] * operands[1],
-                    "/" => operands[0] / operands[1],
-                    "+" => operands[0] + operands[1],
-                    "-" => operands[0] - operands[1],
-                    "^" => operands[0].powf(operands[1] as f64), // might as well just always use float to be safe
-                    // Funcs 
-                    // Trig (radians is assumed):
-                    "sin" => operands[0].sin(),
-                    "cos" => operands[0].cos(),
-                    "tan" => operands[0].tan(),
-                    "abs" => operands[0].abs(),
-                    "arcsin" => operands[0].asin(),
-                    "arccos" => operands[0].acos(),
-                    "arctan" => operands[0].atan(),
-                    "sinh" => operands[0].sinh(),
-                    "cosh" => operands[0].cosh(),
-                    "tanh" => operands[0].tanh(),
-                    "asinh" => operands[0].asinh(),
-                    "acosh" => operands[0].acosh(),
-                    "atanh" => operands[0].atanh(),
-                    // Misc funcs
-                    "floor" => operands[0].floor(),
-                    "ceil" => operands[0].ceil(),
-                    "round" => operands[0].round(),
-                    "trunc" => operands[0].trunc(),
-                    "fract" => operands[0].fract(),
-                    "abs" => operands[0].abs(),
-                    "sqrt" => operands[0].sqrt(),
-                    "cbrt" => operands[0].cbrt(),
-                    "ln" => operands[0].ln(),
-                    "log" => operands[1].log(operands[0]), // assuming argument one is base; the func is number.log(base)
-                    "max" => operands[0].max(operands[1]),
-                    "min" => operands[0].min(operands[1]),
-                    "cbrt" => operands[0].cbrt(),
-                    "exp" => operands[0].exp(),
-                    _ => 0f64,
-                    // Rust's matching syntax means this block of code is a lot better than it _could_ be,
-                    // but it's still pretty bad.
+    while output.len() != 1 { // Process
+        for token in stack.clone() {
+            if token_is_number(token.as_str()) {
+                output.push(token.parse::<f64>().expect("Failed to convert numerical token to f64"));
+            } 
+            
+            else { // The token is an op/func
+                let num_args = match token.as_str() { // How many args does our function/op need?
+                    // Because we're including operators, having one argument is actually outside of the norm:
+                    "sin" | "cos" | "tan" | "ln" | "sqrt" | "cbrt" | "floor" | "ceil" | "round" | "trunc" | "frac" => 1,
+                    // (consdier +, -, *, etc which all need two)
+                    _ => 2, // none of those supported will need 3 as far as I know
                 };
 
-                output.push(result);
+                if output.len() < num_args {
+                    // There was a syntax error; the values needed could not be found
+                    println!("Syntax error encountered: (not enough args for token {}: needed {} but only found {} remaining values in stack).\nThe result will most likely be incorrect.", token, num_args, output.len());
+                    // TODO: handle these errors properly
+                }
+                else {
+                    let mut operands: Vec<f64> = Vec::new();
+                    for _ in 0..num_args {
+                        // Pop n values off of the stack (the range upper limit is exclusive, so we start at 0)               
+                        let operand = output.pop().expect("Failed to pop stack into operands.");
+                        operands.push(operand);
+                    }
+                    operands.reverse(); // For some reason, operands[1] is the first argument when there are multiple variables.
+                    // Evalulate operator for 1-arg functions:
+                    let result: f64 = match token.as_str() {
+                        // This is the equivalent of what an eval() call would do normally.
+                        // Except, we didn't include the "exploit" case, so they can't. Much safer.
+
+                        // At the very least, I've implemented most of the functions provided by f64.
+                        // Most meaning I've excluded those easily done by operators like exp2(self) which returns 2^self. 
+
+                        // That doesn't include ln, sqrt, and cbrt which can easily be done through powers, but are
+                        // still commonplace in most expressions. Those are widely used, so we'll allow them to ensure
+                        // compatibility and ease of use.
+
+                        // Basic ops:
+                        "*" => operands[0] * operands[1],
+                        "/" => operands[0] / operands[1],
+                        "+" => operands[0] + operands[1],
+                        "-" => operands[0] - operands[1],
+                        "^" => operands[0].powf(operands[1] as f64), // might as well just always use float to be safe
+                        // Funcs 
+                        // Trig (radians is assumed):
+                        "sin" => operands[0].sin(),
+                        "cos" => operands[0].cos(),
+                        "tan" => operands[0].tan(),
+                        "abs" => operands[0].abs(),
+                        "arcsin" => operands[0].asin(),
+                        "arccos" => operands[0].acos(),
+                        "arctan" => operands[0].atan(),
+                        "sinh" => operands[0].sinh(),
+                        "cosh" => operands[0].cosh(),
+                        "tanh" => operands[0].tanh(),
+                        "asinh" => operands[0].asinh(),
+                        "acosh" => operands[0].acosh(),
+                        "atanh" => operands[0].atanh(),
+                        // Misc funcs
+                        "floor" => operands[0].floor(),
+                        "ceil" => operands[0].ceil(),
+                        "round" => operands[0].round(),
+                        "trunc" => operands[0].trunc(),
+                        "fract" => operands[0].fract(),
+                        "abs" => operands[0].abs(),
+                        "sqrt" => operands[0].sqrt(),
+                        "cbrt" => operands[0].cbrt(),
+                        "ln" => operands[0].ln(),
+                        "log" => operands[1].log(operands[0]), // assuming argument one is base; the func is number.log(base)
+                        "max" => operands[0].max(operands[1]),
+                        "min" => operands[0].min(operands[1]),
+                        "cbrt" => operands[0].cbrt(),
+                        "exp" => operands[0].exp(),
+                        _ => 0f64,
+                        // Rust's matching syntax means this block of code is a lot better than it _could_ be,
+                        // but it's still pretty bad.
+                    };
+
+                    output.push(result);
+                }
             }
         }
     }
@@ -330,15 +345,5 @@ fn evaluate_postfix(stack: &mut Vec<String>) -> f64 {
         println!("An unknown error occured.");
         return 0f64;
     }
-}
 
-fn stack_trace(stack: &Vec<&str>, output: &Vec<&str>) {
-    print!("RPN Output: ");
-    for token in output {
-        print!("{} ", token);
-    }
-    print!("\nStack: ");
-    for token in stack {
-        print!("{} ", token);
-    }
-}
+
